41bcca214e1b463fd61c0fe5dc1a7062f5f01981
ark <arkan23a@yandex.ru>
2017-06-09 12:01:41 +0300

    second commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+//require('es6-promise').polyfill();
 import fetch from 'isomorphic-fetch';
 import fs from 'fs';
 //import config from './config/index.js';
@@ -21,13 +22,53 @@ function parseFile(){
           }
         });
 
+      });
+      return promise;
+}
+
+
+function parseXMl(){
+    //  let promise=new Promise((resolve,reject)=>{
+        /*fs.readFile("test.html",(err,data)=>{
+
+          let xmlString=data.toString();
+          console.log(xmlString);
+
+          if(err==true){
+            reject();
+          }else {
+            resolve();
+          }
+        });*/
+        var xmlResponse="";
+
+        const response=fetch('http://localhost:4000')
+          .then(function(response) {
+            if (response.status >= 400) {
+                throw new Error("Bad response from server");
+            }
+            //console.log(response);
+            return response.text();
+            })
+          .then(function(stories) {
+              xmlResponse=stories;
+              return xmlResponse;
+            });
+
+        //const data=response.text();
+        //console.log(data);
+        //res.json(data);
+        //res.end(`hello world!!!! ${response}`);
+      //  resolve();
           /*if(true){
               setTimeout(()=>{resolve();console.log("visa accepted")},3000)
           }else{
               reject();
           }*/
-      });
-      return promise;
+    //  });
+    //  return promise;
+      console.log(response);
+      //return response;
 }
 
 async function listenHedelberg() {
@@ -51,6 +92,19 @@ app.get('/', async (req,res)=>{
     res.end(`hello world!!!! ${response}`);
 
 });
+
+app.get('/asyncXML', async (req,res)=>{
+
+    //let data=await listenHedelberg();
+    //const response=await fetch('http://localhost:4000');
+    const response=await fetch('http://localhost:4000');
+    const data=await response.text();
+    console.log(data);
+    res.json(data);
+    res.end(`hello world!!!! ${response}`);
+
+});
+
 app.get('/parseXML', async (req,res)=>{
 
     //let data=await listenHedelberg();
@@ -62,11 +116,26 @@ app.get('/parseXML', async (req,res)=>{
     res.end(`hello world!!!! ${response}`);
 
 });
-app.get('/test', async (req,res)=>{
-    parseFile(); //parse
+app.get('/test',(req,res)=>{
+  //  parseFile(); //parse
+
     //let data=await listenHedelberg();
     //const response=await fetch('http://localhost:4000');
-    res.end(`hello world!!!!`);
+    var xmlResponse="1";
+    const response=fetch('http://localhost:4000')
+      .then(function(response) {
+        if (response.status >= 400) {
+            throw new Error("Bad response from server");
+        }
+        //console.log(response);
+        return response.text();
+        })
+      .then(function(stories) {
+          xmlResponse=stories;
+          console.log(xmlResponse);
+          //res.end(xmlResponse);
+        });
+    res.end(xmlResponse);
 
 });
 app.listen(/*config.get('port')*/3000,()=>{
